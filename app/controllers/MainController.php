8be2d4035f2007c62a6ef1d6331af606484796fc
serrrgun <serrrgun@mail.ru>
2019-05-19 23:40:27 +0300

    Добавляет шаблон главной страницы

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,22 +8,7 @@ class MainController extends AppController {
 
     public function indexAction() {
 
-        //echo __METHOD__;
-
-        $posts = \R::findAll('test');
-        
-        $this->setMeta('Интернет-магазин Luxury', 'Описание', 'Ключевики');
-        $names = ['Sergey', 'Darya', 'Alice', 'Platon'];
-        $cache = Cache::instance();
-        
-        //$cache->delete('test');
-        $data = $cache->get('test');
-        if(!$data) {
-            $cache->set('test', $names);
-        }
-        debug($data);
-        $this->set(compact('posts'));
-        
+       $this->setMeta('Интернет-магазин Luxury', 'Описание', 'Ключевики');
         
     }
 
